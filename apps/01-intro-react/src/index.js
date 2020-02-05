cad715966a27e24a931e7d36c49e64989bce13aa
milenkovorkapic <milenko@hackages.io>
2020-02-05 08:10:57 +0100

    Exercises

---
+++ apps/01-intro-react/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// ===================================================
+import {App} from './1-our-first-component/App';
+// ===================================================
+// import {App} from './2-props/App';
+// ===================================================
+// import {App} from './3-counter/App';
+// ===================================================
+// import {App} from './4-synchronized-counters/App';
+// ===================================================
+// import {App} from './5-conditional-rendering/App';
+// ===================================================
+// import {App} from './6-collection-of-objects/App';
+// ===================================================
+// import {App} from './7-inputs/App';
+// ===================================================
+// import {App} from './8-effects/01-use-effect-1/App';
+// ===================================================
+// import {App} from './8-effects/02-use-effect-2/App';
+// ===================================================
+// import {App} from './9-context/App';
+// ===================================================
+// import {App} from './10-useReducer/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
